8f59289e0eea8a3442c8c40b46d189cbc6dfa5c2
Section <anthony.chew@hotmail.com>
2022-10-24 10:35:19 +0800

    Added TextDB.class and DateTIme class

--- src/Cinema.java
+++ src/Cinema.java
@@ -11,7 +11,7 @@ public class Cinema {
         this.cinemaName = cinemaName;
     }
 
-    public AddNewMovies()
+    //public AddNewMovies()
 
     public String getCinemaName() {
         return this.cinemaName;
