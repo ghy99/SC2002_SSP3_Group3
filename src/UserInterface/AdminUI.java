de551380419bc453da83e962cffc90129fc901d7
Tttjjjlll <115860601+Tttjjjlll@users.noreply.github.com>
2022-10-29 21:23:28 +0800

    juelin

--- src/UserInterface/AdminUI.java
+++ src/UserInterface/AdminUI.java
@@ -9,52 +9,51 @@ public class AdminUI {
 	
 	public void login() throws IOException {
 		
-		ArrayList<Admin> adminList = new ArrayList<Admin>();
+		//fetch data of admin info from txt storage
+		ArrayList<Admin> emptyAdminList = new ArrayList<Admin>();
 	    TextDB textDB = new TextDB();
-	    ArrayList<Admin> filledAdminList = textDB.ReadFromFile(adminList, "admin.txt");
-	    
-	    for (int i = 0; i<3;i++) {
-	    	System.out.println(filledAdminList.get(i).getName()+ filledAdminList.get(i).getPassword());
-	    }
+	    ArrayList<Admin> filledAdminList = textDB.ReadFromFile(emptyAdminList, "admin.txt");
 	    
 	    
+	    //for debugging checking output
+//	    for (int i = 0; i<filledAdminList.size();i++) {
+//	    	System.out.println(filledAdminList.get(i).getName()+ filledAdminList.get(i).getPassword());
+//	    }
 	    
+	    boolean flag = true;
 	    
-//		System.out.println("Please Provide UserName");
-//		Scanner scan = new Scanner(System.in);
-//		String userName = scan.nextLine();
-//		System.out.println("Please Provide Password");
-//		String passWord = scan.nextLine();
-
-    
-    
+	    do{
+	    	
+			System.out.println("Please Provide Username");
+			Scanner scan = new Scanner(System.in);
+			String userName = scan.nextLine();
+			System.out.println("Please Provide Password");
+			String passWord = scan.nextLine();
+			
+			String dataName; //can use .equals() because string is an object
+			String dataPassword;
+			for (int i = 0; i<filledAdminList.size();i++) {
+				dataName = filledAdminList.get(i).getName();
+				dataPassword = filledAdminList.get(i).getPassword();
+				if(userName.equals(dataName) && passWord.equals(dataPassword)) {
+					System.out.println("Welcome " + dataName + ". You have logged in successfully to the admin portal.");
+					return;
+				}
+			}
+			
+			System.out.println("Incorrect Username or Password.");
+			System.out.println("You may enter 1 to re-login or Enter 0 if you dont want to log in anymore.");
+			int temp = scan.nextInt();
+			if(temp==0) {
+				flag = false;
+				System.out.println("Goodbye!");
+			}
+			
 		
-	// 	main->customerUI,Admininterface ->admininterface object, login
-	// 	log in fn first 
-	// while 
-		
-		//check if in text file kick them out to back to main
-		//if user name and password is in text file
-//		System.out.println("Hello " + userName + "Welcome Back to the Admin Portal");
-//		
-//		//else if userName is in text file but not password, alr have an account
-//		System.out.println("Hello " + userName + "Incorrect Password. Please try log in again");
-//		
-//		
-//		//check both username and password is in text file AGAIN
-//		
-//		//else create new admin account
-//		System.out.println("Please create new UserName");
-//		String userName = scan.nextLine();
-//		System.out.println("Please create new Password");
-//		String passWord = scan.nextLine();
-//		//pass info into text file
-//		
-//		
-//		Admin admin1 = new Admin("Apple","Password");
-//		
-//		
+	    }while (flag);	
 	}
+	
+	
 	
 }
 
