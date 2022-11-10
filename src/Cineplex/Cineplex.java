da26e3bf9e0413f1af42936a45d06b57e764b542
Section <anthony.chew@hotmail.com>
2022-11-10 13:30:09 +0800

    Update Cineplex.java

--- src/Cineplex/Cineplex.java
+++ src/Cineplex/Cineplex.java
@@ -148,31 +148,38 @@ public class Cineplex {
         for (int j = 0; j < movielist.size(); j++) {
             System.out.printf("\n\t%s\n", movielist.get(j).getMovieTitle()); // movie
             ArrayList<ShowTime> allST = new ArrayList<>();
+            ArrayList<Cinema> cinemas = new ArrayList<>();
             for (Cinema c : listOfCinemas) {
                 ArrayList<ShowTime> listOfShowtime = c.getShowTime();
                 if (listOfShowtime.size() > 0) {
                     for (ShowTime st : listOfShowtime) {
                         if (Objects.equals(st.getMovie().getMovieTitle(), movielist.get(j).getMovieTitle())) {
                             allST.add(st);
+                            cinemas.add(c);
+                            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+                            System.out.printf("%s:%s \n", st.getMovie().getMovieTitle(), timeFormat.format(st.getTime()));
                         }
                     }
                 }
             }
+
+
             if (allST.size() > 0) {
-                allST.sort(Comparator.comparing(ShowTime::getTimeHour));
+                //allST.sort(Comparator.comparing(ShowTime::getTimeHour));
                 // need a sort function
                 SimpleDateFormat dateFormat = new SimpleDateFormat("EEE dd-MM-YYYY");
                 SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
                 String previousString = dateFormat.format(allST.get(0).getTime());
                 System.out.printf("\t\t%s", previousString);
                 for (int k = 0; k < allST.size(); k++) {
+                    System.out.printf("\tCinema %s", cinemas.get(k).getCinemaName());
                     if (Objects.equals(previousString, dateFormat.format(allST.get(k).getTime()))) {
                         previousString = "";
-                        System.out.printf("%s\t%s", previousString, timeFormat.format(allST.get(k).getTime()));
+                        System.out.printf("%s:%s", previousString, timeFormat.format(allST.get(k).getTime()));
                         previousString = dateFormat.format(allST.get(k).getTime());
                     } else {
                         previousString = dateFormat.format(allST.get(k).getTime());
-                        System.out.printf("\n\t\t%s\t%s", previousString, timeFormat.format(allST.get(k).getTime()));
+                        System.out.printf("\n\t\t%s\tCinema %s:%s", previousString, cinemas.get(k).getCinemaName(), timeFormat.format(allST.get(k).getTime()));
                     }
                 }
                 System.out.println();
